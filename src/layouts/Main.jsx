ff5da9aa2d670ff8f729c727737e9d2dc0527a9a
zihad <zynscriptofficial@gmail.com>
2023-06-07 14:53:55 +0600

    footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../pages/Shared/NavBar";
 import {Outlet} from "react-router-dom";
+import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
   return (
     <div>
       <NavBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/Shared/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="h-screen">
+      <footer className="footer p-10 bg-base-200 text-base-content fixed bottom-0">
+        <div>
+          <Link to="/" className="text-3xl font-bold">
+            SpeedyToys
+          </Link>
+          <p>
+            Speedy Toys Ltd.
+            <br />
+            Copyright &copy; 2023 SpeedyToys.
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">PAGES</span>
+          <Link to="/" className="link link-hover">
+            Home
+          </Link>
+          <Link to="/" className="link link-hover">
+            All Toys
+          </Link>
+          <Link to="/" className="link link-hover">
+            My Toys
+          </Link>
+          <Link to="/" className="link link-hover">
+            Add A Toy
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
